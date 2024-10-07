b9dabf2ba37f12032d62f61109759775350cb3e5
wafiy@anak.it <wafiy@anak.it>
2024-10-07 10:45:18 +0800

    adapt plain PHP code to fit MVC

--- index.php
+++ index.php
@@ -1,16 +1,10 @@
 <?php
 
-   $dsn = "mysql:host=localhost;dbname=product_db;charset=utf8;port=3306";
-   
-   $pdo = new PDO($dsn, "product_db_user", "pass123", [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-   ]);
+   require "model.php";
 
-   $stmt = $pdo->query("SELECT * FROM product");
+   $model = new Model;
 
-   $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-//    print_r($products);
+   $products = $model->getData();
 
 ?>
 <!DOCTYPE html>
---
+++ model.php
@@ -0,0 +1,18 @@
+<?php
+
+class Model{
+
+    public function getData(): array
+    {
+        $dsn = "mysql:host=localhost;dbname=product_db;charset=utf8;port=3306";
+   
+        $pdo = new PDO($dsn, "product_db_user", "pass123", [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]);
+
+        $stmt = $pdo->query("SELECT * FROM product");
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+}
