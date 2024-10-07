f46632aeab171f9ebe73f6976221b6e7d0e2aedf
wafiy@anak.it <wafiy@anak.it>
2024-10-07 10:47:26 +0800

    create view class

--- index.php
+++ index.php
@@ -6,21 +6,4 @@
 
    $products = $model->getData();
 
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Products</title>
-</head>
-<body>
-
-    <h1>Products</h1>
-    <?php foreach ($products as $product): ?>
-        <h2><?= htmlspecialchars($product['name']) ?></h2>
-        <p><?= htmlspecialchars($product['description']) ?></p>
-    <?php endforeach; ?>
-
-</body>
-</html>
+   require "view.php";
