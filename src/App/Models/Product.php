212621a659d29f573c9df76a1a60c6ee36f54f7f
wafiy@anak.it <wafiy@anak.it>
2024-10-07 16:32:30 +0800

    add comments in product model

--- src/App/Models/Product.php
+++ src/App/Models/Product.php
@@ -2,21 +2,21 @@
 
 namespace App\Models;
 
-use PDO;
+use PDO; // Importing the PDO class from the global namespace
 
-class Product{
+class Product {
 
     public function getData(): array
     {
+        // You can directly use 'PDO' because of the 'use' statement
         $dsn = "mysql:host=localhost;dbname=product_db;charset=utf8;port=3306";
    
         $pdo = new PDO($dsn, "product_db_user", "pass123", [
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
         ]);
 
         $stmt = $pdo->query("SELECT * FROM product");
 
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
-
 }
