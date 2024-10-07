2a717f5234ad43ec6f4881c075fa4acd7f07df62
wafiy@anak.it <wafiy@anak.it>
2024-10-07 14:13:17 +0800

    edit index as the front controller

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
 
-   require "src/controllers/products.php";
-   $controller = new Products();
-   $controller->index();
+    require "src/controllers/products.php";
+    $controller = new Products();
+
+    $action = $_GET["action"];
+
+    if ($action === "index") {
+        # code...
+        $controller->index();
+    } elseif ($action === "show") {
+        # code...
+        $controller->show();
+    } else{
+        $controller->index();
+    }
+    
