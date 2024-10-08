b3fe075e361c60f9d41f861c86f28426d7d48697
wafeeyhm <wafiy@anak.it>
2024-10-08 08:59:41 +0800

    passed the captured controller and action to the front controller

--- src/Framework/Router.php
+++ src/Framework/Router.php
@@ -6,7 +6,7 @@ class Router{
 
     private array $routes = [];
 
-    public function add(string $path, array $params):void {
+    public function add(string $path, array $params = []):void {
 
         $this->routes[] = [
             "path" => $path,
