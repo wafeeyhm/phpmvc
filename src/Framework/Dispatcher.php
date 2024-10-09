d1984d3d5795f73c7d22cbde10283843b294b490
wafeeyhm <wafiy@anak.it>
2024-10-09 10:20:49 +0800

    one of the way to do this

--- src/Framework/Dispatcher.php
+++ src/Framework/Dispatcher.php
@@ -3,5 +3,12 @@
 namespace Framework;
 
 class Dispatcher{
-    
+
+    private Router $router;
+
+    public function __construct(Router $router)
+    {
+        $this->router = $router;
+    }
+
 }
