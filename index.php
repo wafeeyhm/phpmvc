43b0b147555b32e94939b4bf6eb39ac6232623d9
wafeeyhm <wafiy@anak.it>
2024-10-09 09:20:14 +0800

    add general routes

--- index.php
+++ index.php
@@ -10,12 +10,12 @@ spl_autoload_register(function(string $class_name){
 
 $router = new Framework\Router;
 
-// $router->add("/{controller}/{action}");
 $router->add("/products/{slug:[\w-]+}", ["controller" => "products", "action" => "show"]);
 $router->add("/{controller}/{id:\d+}/{action}");
 $router->add("/home/index" , ["controller" => "home", "action" => "index"]);
 $router->add("/products" , ["controller" => "products", "action" => "index"]);
 $router->add("/" , ["controller" => "home", "action" => "index"]);
+$router->add("/{controller}/{action}");
 
 $params = $router->match($path);
 
