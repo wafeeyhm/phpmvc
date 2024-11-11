13f39ca42bed9f919e094e075d87e51479d5eb18
wafeeyhm <wafiy@anak.it>
2024-11-11 17:20:45 +0800

    generate the sql dynamically based on the name, number of columns

--- src/Framework/Model.php
+++ src/Framework/Model.php
@@ -58,7 +58,14 @@ abstract class Model
 
     public function insert(array $data): bool
     {
-        $sql = "INSERT INTO product (name, description) VALUES (?,?)";
+
+        $columns = implode(",", array_keys($data));
+
+        $placeholder = implode(", ", array_fill(0, count($data), "?"));
+
+        $sql = "INSERT INTO {$this->getTable()} ($columns) VALUES ($placeholder)";
+
+        exit($sql);
 
         $conn = $this->database->getConnection();
 
