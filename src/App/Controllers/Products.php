b224aa9aa2588c33b38811e1c40358a7fed35aff
wafiy@anak.it <wafiy@anak.it>
2024-11-12 12:30:05 +0800

    redisplay the form with the error message for invalid data

--- src/App/Controllers/Products.php
+++ src/App/Controllers/Products.php
@@ -74,8 +74,22 @@ class Products{
             "description" => empty($_POST["description"]) ? null : $_POST["description"],
         ];
 
-        var_dump($this->model->insert($data));
+        if($this->model->insert($data)){
 
-        print_r($this->model->getErrors());
+            echo "record saved.";
+
+        } else {
+
+            echo $this->viewer->render("shared/header.php",[
+                "title" => "New Product"
+            ]);
+    
+            echo $this->viewer->render("Products/new.php",[
+                "errors" => $this->model->getErrors()
+            ]);
+    
+            echo $this->viewer->render("shared/footer.php");
+
+        }
     }
 }
--- views/Products/new.php
+++ views/Products/new.php
@@ -4,6 +4,10 @@
     <label for="name">Name</label>
     <input type="text" id="name" name="name">
 
+    <?php if (isset($errors["name"])): ?>
+        <p><?= $errors["name"] ?></p>
+    <?php endif; ?>
+
     <label for="description">Description</label>
     <textarea id="description" name="description" rows="3"></textarea>
 
