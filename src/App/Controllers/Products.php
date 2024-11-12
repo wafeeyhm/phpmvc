8741beea7afd8fc85699fc6c5a66977befb222ba
wafiy@anak.it <wafiy@anak.it>
2024-11-12 13:25:55 +0800

    redisplay the prev data when redisplaying the form

--- src/App/Controllers/Products.php
+++ src/App/Controllers/Products.php
@@ -86,7 +86,8 @@ class Products{
             ]);
     
             echo $this->viewer->render("Products/new.php",[
-                "errors" => $this->model->getErrors()
+                "errors" => $this->model->getErrors(),
+                "product" => $data
             ]);
     
             echo $this->viewer->render("shared/footer.php");
@@ -124,12 +125,10 @@ class Products{
             throw new PageNotFoundException("Product not found");
         }
 
-        $data = [
-            "name" => $_POST["name"],
-            "description" => empty($_POST["description"]) ? null : $_POST["description"],
-        ];
+        $product["name"] = $_POST["name"];
+        $product["description"] = empty($_POST["description"]) ? null : $_POST["description"];
 
-        if($this->model->update($id, $data)){
+        if($this->model->update($id, $product)){
 
             header("Location: /products/{$id}/show");
             exit;
