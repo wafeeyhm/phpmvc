e42afae5f7103ffc0bb64708d3a20f78f07e2b85
wafiy@anak.it <wafiy@anak.it>
2024-11-14 16:59:26 +0800

    use alternative syntax for control structure

--- src/Framework/MVCTemplateViewer.php
+++ src/Framework/MVCTemplateViewer.php
@@ -8,14 +8,28 @@ class MVCTemplateViewer implements TemplateViewerInterface
 {
     public function render(string $template, array $data = []): string
     {
+        $code = file_get_contents(dirname(__DIR__, 2) . "/views/$template");
+
+        $code = $this->replaceVariables($code);
+
+        $code = $this->replacePHP($code);
 
         extract($data, EXTR_SKIP);
 
         ob_start();
 
-        require dirname(__DIR__, 2) . "/views/$template";
+        eval("?>$code");
 
         return ob_get_clean();
+    }
 
+    private function replaceVariables(string $code): string
+    {
+        return preg_replace("#{{\s*(\S+)\s*}}#", "<?= htmlspecialchars(\$$1) ?>", $code);
+    }
+
+    private function replacePHP(string $code): string
+    {
+        return preg_replace("#{%\s*(.+)\s*%}#", "<?php $1 ?>", $code);
     }
 }
--- views/Products/index.mvc.php
+++ views/Products/index.mvc.php
@@ -3,27 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $title ?></title>
+    <title>{{ title }}</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
 </head>
 <body>
 <h1>Products</h1>
 <a href="/products/new">New Product</a>
-<p><?= $total; ?></p>
+<p>Total: {{ total }}</p>
 <table class="table">
     <thead>
         <th scope="col">Name</th>
     </thead>
     <tbody>
-    <?php foreach ($products as $product): ?>
+    {% foreach ($products as $product): %}
         <tr>
         <td>
             <a href="/products/<?= $product["id"]?>/show">
-            <?= htmlspecialchars($product['name']) ?>
+            {{ product["name"] }}
             </a>
         </td> 
         </tr>
-    <?php endforeach; ?>
+    {% endforeach; %}
     </tbody>
 </table>
 </body>
